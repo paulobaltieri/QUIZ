d200ec2fe2f670a7e69de68ed27b56c2e604039f
Paulo Baltieri <37188572+paulobaltieri@users.noreply.github.com>
2022-08-31 15:23:10 -0300

    Adição de eventos ao javascript

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,15 @@
 const form = document.querySelector('.questions')
 const correctQuestion = ['b', 'b', 'b', 'b']
 
+form.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const userQuestions = [
+        form.inputQuestion1.value,
+        form.inputQuestion2.value,
+        form.inputQuestion3.value,
+        form.inputQuestion4.value,
+    ]
+    console.log(userQuestions)
+})
+
